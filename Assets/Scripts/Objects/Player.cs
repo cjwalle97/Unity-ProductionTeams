2bf163815ea2531ae681383a9fb84e9d12c3a337
Trent Butler <redtrentbutler@gmail.com>
2017-05-25 14:46:50 -0500

    'fix'

---
+++ Assets/Scripts/Objects/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player")]
+public class Player : ScriptableObject
+{
+    public string Name;
+    public float Health;
+    public float Damage;
+    public float MovementSpeed;
+    public bool Alive;
+}
