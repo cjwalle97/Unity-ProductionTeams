cd92d9dac6e5cfb46f743cc1acac667cafee9d83
Trent Butler <redtrentbutler@gmail.com>
2017-05-26 17:59:22 -0500

    add 'playerbehaviour' script

---
+++ Assets/Scripts/Behaviors/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+
+    //NEEDS WORK
+    public Player Player;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //SETUP MOVEMENT
+        //SETUP CAMERA FOR PLAYER
+
+	}
+}
