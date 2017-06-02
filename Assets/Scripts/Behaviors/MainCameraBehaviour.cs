2550a065687ed6b4f6cc428ab644c3153cb9250b
Trent Butler <redtrentbutler@gmail.com>
2017-06-02 12:22:42 -0500

    add 'maincamerabehaviour'

---
+++ Assets/Scripts/Behaviors/MainCameraBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraBehaviour : MonoBehaviour {
+
+    private Transform _playertransform;
+    private Camera _maincamera;
+	// Use this for initialization
+	void Start () {
+        _playertransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _maincamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        _playertransform = GameObject.FindGameObjectWithTag("Player").transform;        
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 offset = _playertransform.position;
+        Vector3 calc_offset = new Vector3(0, 12, -6);
+        offset += calc_offset;
+        _maincamera.transform.position = offset;
+        _maincamera.transform.rotation = Quaternion.AngleAxis(45, Vector3.right);
+    }
+}
--- Assets/Scripts/Behaviors/PlayerBehaviour.cs
+++ Assets/Scripts/Behaviors/PlayerBehaviour.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-
+    private float _speed = 20;
     //NEEDS WORK
     public Player Player;
 
@@ -15,9 +15,11 @@ public class PlayerBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
         //SETUP MOVEMENT
         //SETUP CAMERA FOR PLAYER
-
-	}
+        var h = Input.GetAxis("Horizontal");
+        var v = Input.GetAxis("Vertical");
+        transform.localPosition += new Vector3(h, 0, v) * _speed * Time.deltaTime;
+    }
 }
