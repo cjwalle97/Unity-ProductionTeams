ed7c6b2d446387db044ef5bbbcbbd326f73d9d00
cjwalle97 <cjwalle97@gmail.com>
2017-06-07 16:57:22 -0500

    Update
    Update

---
+++ Assets/Scripts/Behaviors/AttackTransitionBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AttackTransitionBehaviour : StateMachineBehaviour
+{
+    public GameObject EnemyGameObject;
+    public GameObject Target;
+    public NavMeshAgent Agent;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+
+    //    Vector3 distance = EnemyGameObject.transform.position - Target.transform.position;
+
+
+
+        
+    //    if ()
+    //}
+    
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
--- Assets/Scripts/Behaviors/EnemyBehavior.cs
+++ Assets/Scripts/Behaviors/EnemyBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyBehaviour : MonoBehaviour {
+public class EnemyBehavior : MonoBehaviour {
 
     public NavMeshAgent Agent;
     public Transform Target;
