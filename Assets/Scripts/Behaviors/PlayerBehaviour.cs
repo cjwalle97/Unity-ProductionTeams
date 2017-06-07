2014f62b500d21151f5343c5dba6449c8ebe95bd
Trent Butler <redtrentbutler@gmail.com>
2017-06-07 15:41:13 -0500

    begin implementation on player 'aiming'

--- Assets/Scripts/Behaviors/PlayerBehaviour.cs
+++ Assets/Scripts/Behaviors/PlayerBehaviour.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour {
 
     private float _speed = 20;
+    private float _lookspeed = 10;
     //NEEDS WORK
     public Player Player;
 
@@ -12,14 +13,36 @@ public class PlayerBehaviour : MonoBehaviour {
 	void Start () {
         
 	}
+
+
+    Vector3 LookAround()
+    {
+        var _hori = Input.GetAxis("HorizontalArrow");
+        var _vert = Input.GetAxis("VerticalArrow");
+
+        Vector3 _direction = new Vector3(_hori, 0, _vert);        
+
+        return _direction * _lookspeed * Time.deltaTime;
+    }
+
+
 	
 	// Update is called once per frame
 	void Update () {
 
         //SETUP MOVEMENT
         //SETUP CAMERA FOR PLAYER
+        
+    }
+
+    void FixedUpdate()
+    {
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
+
         transform.localPosition += new Vector3(h, 0, v) * _speed * Time.deltaTime;
+
+        transform.localRotation = Quaternion.LookRotation(LookAround());
     }
+
 }
