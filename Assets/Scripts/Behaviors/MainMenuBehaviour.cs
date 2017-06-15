5e3486e3420b5b800fb75e1eba46e1886b5d45a3
Christopher Walle <cjwalle97@gmail.com>
2017-06-15 16:03:24 -0500

    Main Menu and Game Over Menu
    Adds the Main Menu and Game Over Scenes as well as the canvases
    associated with them.

---
+++ Assets/Scripts/Behaviors/MainMenuBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuBehaviour : MonoBehaviour
+{
+    public Text ControlsText;
+    public Text ArtistsText;
+    public Text ProgrammersText;
+    
+    void Start()
+    {
+        ControlsText.text = "";
+        ArtistsText.text = "";
+        ProgrammersText.text = "";
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("0.main", LoadSceneMode.Additive);
+    }
+
+    public void DisplayControls()
+    {
+        ControlsText.text = "@Trent add the controlls bruhhhh!!!!";
+    }
+
+    public void DisplayCredits()
+    {
+        ArtistsText.text = "ARTISTS: " + "Bruce Mayo, " + "Joshau Cambre, " + "Blaise Badon";
+        ProgrammersText.text = "PROGRAMMERS: " + "Trent Butler, " + "Christopher Walle";
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
