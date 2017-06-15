25e34ccae6adc25131f1c6d26e3ad90d599f113f
Christopher Walle <cjwalle97@gmail.com>
2017-06-15 10:33:23 -0500

    PlayerAttacker Prefab
    Refactors the PlayerAttacker Prefab and also the
    PlayerAttackerBehaviour. Adds JunkBulletBehaviour

---
+++ Assets/Scripts/Behaviors/JunkBulletBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JunkBulletBehaviour : MonoBehaviour
+{
+
+    private Enemy _shooter;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    _shooter = GetComponent<EnemyBehavior>().EnemyConfig;
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _shooter.DoDamage(other.GetComponent<PlayerBehaviour>()._player);
+        }
+        if (other.tag == "Tower")
+        {
+            _shooter.DoDamage(other.GetComponent<TowerBehaviour>()._tower);
+        }
+    }
+}
